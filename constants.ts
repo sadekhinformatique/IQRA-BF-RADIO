395162b0aa1fd13b5861b6acaf2e92d04bd68167
sadekhinformatique <155910956+sadekhinformatique@users.noreply.github.com>
2025-12-15 17:44:22 +0000

    Update logo URL and stream URLs in constants

--- constants.ts
+++ constants.ts
@@ -3,24 +3,24 @@ import { AppConfig } from './types';
 export const DEFAULT_CONFIG: AppConfig = {
   radioName: "RADIO IQRA BF",
   primaryColor: "#10B981", // Emerald-500
-  logoUrl: "https://picsum.photos/400/400", // Placeholder
+  logoUrl: "https://i.pinimg.com/1200x/f6/d3/f6/f6d3f61ec150126bb68dcd3fc45aa508.jpg", // Placeholder
   streams: [
     {
       id: '1',
       title: 'Main Stream (Quran)',
-      url: 'https://stream.zeno.fm/0r0xa792kwzuv', // Example stream
+      url: 'https://stream.zeno.fm/ztmkyozjspltv', // Example stream
       active: true,
     },
     {
       id: '2',
       title: 'Backup Stream 1',
-      url: 'https://stream.zeno.fm/v3p6454402quv', // Example stream
+      url: 'https://stream.zeno.fm/ztmkyozjspltv.m3u', // Example stream
       active: true,
     },
     {
       id: '3',
       title: 'Backup Stream 2',
-      url: 'https://l3.itworkscdn.net/itwaudio/9006/stream', // Example stream
+      url: 'https://stream.zeno.fm/ztmkyozjspltv.pls', // Example stream
       active: true,
     }
   ]
