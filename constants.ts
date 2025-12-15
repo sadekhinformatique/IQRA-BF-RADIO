417df2df32bcdd38914971a97757b486a7939c7a
sadekhinformatique <155910956+sadekhinformatique@users.noreply.github.com>
2025-12-15 18:35:52 +0000

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -3,9 +3,9 @@ import { AppConfig } from './types';
 export const DEFAULT_CONFIG: AppConfig = {
   radioName: "RADIO IQRA BF",
   primaryColor: "#10B981", // Emerald-500
-  logoUrl: "https://picsum.photos/400/400", // Placeholder
+  logoUrl: "https://i.pinimg.com/1200x/f6/d3/f6/f6d3f61ec150126bb68dcd3fc45aa508.jpg", // Placeholder
   streamTitle: "Direct",
-  streamUrl: "https://stream.zeno.fm/0r0xa792kwzuv", // Example stream
+  streamUrl: "https://stream.zeno.fm/ztmkyozjspltv.m3u", // Example stream
 };
 
 export const STORAGE_KEY = 'radio_iqra_config_v2';
