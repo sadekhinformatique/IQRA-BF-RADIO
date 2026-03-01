959ac494f9ebc76980a9e24332e6122d5dd8700d
sadekhinformatique <155910956+sadekhinformatique@users.noreply.github.com>
2026-03-01 22:34:00 +0000

    Update stream URL for radio station

--- constants.ts
+++ constants.ts
@@ -5,7 +5,7 @@ export const DEFAULT_CONFIG: AppConfig = {
   primaryColor: "#10B981", // Emerald-500
   logoUrl: "https://i.pinimg.com/1200x/f6/d3/f6/f6d3f61ec150126bb68dcd3fc45aa508.jpg",
   streamTitle: "Direct",
-  streamUrl: "https://stream.zeno.fm/ztmkyozjspltv", 
+  streamUrl: "https://radioiqrabf-1.radiohls.infomaniak.com/radioiqrabf-1/manifest.m3u8", 
 };
 
 export const STORAGE_KEY = 'radio_iqra_config_v2';
